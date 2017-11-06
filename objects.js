f34a6b45785ec65feb08ee04335655c7ea586077
Patrick Gallagher <pgallagher85@gmail.com>
2017-11-06 13:11:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,5 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, 'artistName', 'songTitle') {
-  playlist.artistName = 'songTitle'
-  return playlist
+  return playlist.artistName = 'songTitle'
 }
